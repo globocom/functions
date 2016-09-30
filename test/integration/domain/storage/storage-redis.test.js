21f976e4d5509fa9af2cdb385356ccfef9b4b157
Wilson Júnior <wilsonpjunior@gmail.com>
2016-09-30 11:12:41 -0300

    Run eslint --fix lib test

--- test/integration/domain/storage/storage-redis.test.js
+++ test/integration/domain/storage/storage-redis.test.js
@@ -5,233 +5,233 @@ const StorageRedis = require('../../../../lib/domain/storage/redis');
 const config = require('../../../../lib/support/config');
 
 describe('StorageRedis', () => {
-    let storage;
+  let storage;
 
-    before(() => {
-        let newOptions = deepcopy(config.redis);
-        newOptions.enableOfflineQueue = true;
-        newOptions.keyPrefix = 'test:';
+  before(() => {
+    const newOptions = deepcopy(config.redis);
+    newOptions.enableOfflineQueue = true;
+    newOptions.keyPrefix = 'test:';
 
-        storage = new StorageRedis(newOptions);
+    storage = new StorageRedis(newOptions);
+  });
+
+  describe('#ping()', () => {
+    it('should return pong', (done) => {
+      storage.ping().then((res) => {
+        expect(res).to.be.eql('PONG');
+        done();
+      }, (err) => {
+        done(err);
+      });
     });
-
-    describe('#ping()', () => {
-        it('should return pong', (done) => {
-            storage.ping().then((res) => {
-                expect(res).to.be.eql('PONG');
-                done();
-            }, (err) => {
-                done(err);
-            });
-        });
-    });
+  });
 
 
-    describe('#putCode() and #getCode()', () => {
-        it('should write a hash for the code', (done) => {
-            let code = {
-                id: 'test',
-                code: 'a = 1;',
-                hash: '123',
-            };
-            storage.putCode('backstage', 'test', code)
+  describe('#putCode() and #getCode()', () => {
+    it('should write a hash for the code', (done) => {
+      const code = {
+        id: 'test',
+        code: 'a = 1;',
+        hash: '123',
+      };
+      storage.putCode('backstage', 'test', code)
                 .then((x) => {
-                    expect(x).to.be.eql('OK');
-                    return storage.getCode('backstage', 'test');
+                  expect(x).to.be.eql('OK');
+                  return storage.getCode('backstage', 'test');
                 })
                 .then((code) => {
-                    expect(code.id).to.be.eql('test');
-                    expect(code.code).to.be.eql('a = 1;');
-                    expect(code.hash).to.be.eql('123');
-                    done();
+                  expect(code.id).to.be.eql('test');
+                  expect(code.code).to.be.eql('a = 1;');
+                  expect(code.hash).to.be.eql('123');
+                  done();
                 }, (err) => {
-                    done(err);
+                  done(err);
                 });
+    });
+  });
+
+
+  describe('#getCode()', () => {
+    describe('when code id not is found', () => {
+      it('should yield a null', (done) => {
+        storage.getCode('backstage', 'not-found').then((code) => {
+          expect(code).to.be.null;
+          done();
+        }, (err) => {
+          done(err);
         });
+      });
     });
 
+        // when code is found is already tested above
+  });
 
-    describe('#getCode()', () => {
-        describe('when code id not is found', () => {
-            it('should yield a null', (done) => {
-                storage.getCode('backstage', 'not-found').then((code) => {
-                    expect(code).to.be.null;
-                    done();
-                }, (err) => {
-                    done(err);
-                });
-            });
-        });
-
-        //when code is found is already tested above
-    });
-
-    describe('#delete()', () => {
-        it('should write a hash for the code', (done) => {
-            let id = 'test';
-            let namespace = 'backstage';
-            let code = {
-                id,
-                code: 'a = 1;',
-                hash: '123',
-            };
-            storage.putCode(namespace, id, code)
+  describe('#delete()', () => {
+    it('should write a hash for the code', (done) => {
+      const id = 'test';
+      const namespace = 'backstage';
+      const code = {
+        id,
+        code: 'a = 1;',
+        hash: '123',
+      };
+      storage.putCode(namespace, id, code)
                 .then((putResponse) => {
-                    expect(putResponse).to.be.eql('OK');
-                    return storage.deleteCode(namespace, id);
+                  expect(putResponse).to.be.eql('OK');
+                  return storage.deleteCode(namespace, id);
                 })
                 .then((deleteResponse) => {
-                    expect(deleteResponse).to.be.eql(1);
-                    return storage.getCode(namespace, id);
+                  expect(deleteResponse).to.be.eql(1);
+                  return storage.getCode(namespace, id);
                 })
                 .then((code) => {
-                    expect(code).to.be.null;
-                    done();
-                } , (err) => {
-                    done(err);
+                  expect(code).to.be.null;
+                  done();
+                }, (err) => {
+                  done(err);
                 });
-        });
+    });
+  });
+
+  describe('#getCodeByCache', () => {
+    describe('when code is not found in storage', () => {
+      it('should populate item by preCache', (done) => {
+        const preCache = (code) => {
+          code.neverCalled = true;
+          return code;
+        };
+
+        const namespace = 'backstage';
+        const id = 'cache-000';
+
+        storage
+                    .getCodeByCache(namespace, id, { preCache })
+                    .then((cacheResponse) => {
+                      expect(cacheResponse).to.be.null;
+                      done();
+                    }, (err) => {
+                      done(err);
+                    });
+      });
     });
 
-    describe('#getCodeByCache', () => {
-        describe('when code is not found in storage', () => {
-            it('should populate item by preCache', (done) => {
-                let preCache = (code) => {
-                    code.neverCalled = true;
-                    return code;
-                };
+    describe('when code is not found in cache', () => {
+      it('should populate item by preCache', (done) => {
+        const preCache = (code) => {
+          code.preCached = true;
+          preCache.called = true;
+          return code;
+        };
 
-                let namespace = 'backstage';
-                let id = 'cache-000';
+        const namespace = 'backstage';
+        const id = 'cache-01';
+        const code = {
+          id,
+          code: 'b = 1;',
+          hash: '123',
+        };
 
-                storage
-                    .getCodeByCache(namespace, id, {preCache})
-                    .then((cacheResponse) => {
-                        expect(cacheResponse).to.be.null;
-                        done();
-                    }, (err) => {
-                        done(err);
-                    });
-            });
-        });
-
-        describe('when code is not found in cache', () => {
-            it('should populate item by preCache', (done) => {
-                let preCache = (code) => {
-                    code.preCached = true;
-                    preCache.called = true;
-                    return code;
-                };
-
-                let namespace = 'backstage';
-                let id = 'cache-01';
-                let code = {
-                    id,
-                    code: 'b = 1;',
-                    hash: '123',
-                };
-
-                storage
+        storage
                     .putCode(namespace, id, code)
                     .then((putResponse) => {
-                        expect(putResponse).to.be.eql('OK');
-                        return storage.getCodeByCache(namespace, id, {preCache});
+                      expect(putResponse).to.be.eql('OK');
+                      return storage.getCodeByCache(namespace, id, { preCache });
                     })
                     .then((cacheResponse) => {
-                        expect(cacheResponse.preCached).to.be.true;
-                        expect(preCache.called).to.be.true;
-                        done();
+                      expect(cacheResponse.preCached).to.be.true;
+                      expect(preCache.called).to.be.true;
+                      done();
                     }, (err) => {
-                        done(err);
+                      done(err);
                     });
-            });
-        });
+      });
+    });
 
-        describe('when code is found in cache and not changed', () => {
-            it('should return item by cache', (done) => {
-                let preCache = (code) => {
-                    code.preCached = true;
-                    preCache.called = true;
-                    return code;
-                };
+    describe('when code is found in cache and not changed', () => {
+      it('should return item by cache', (done) => {
+        const preCache = (code) => {
+          code.preCached = true;
+          preCache.called = true;
+          return code;
+        };
 
-                let namespace = 'backstage';
-                let id = 'cache-02';
-                let code = {
-                    id,
-                    code: 'b = 1;',
-                    hash: '123a',
-                };
+        const namespace = 'backstage';
+        const id = 'cache-02';
+        const code = {
+          id,
+          code: 'b = 1;',
+          hash: '123a',
+        };
 
-                storage
+        storage
                     .putCode(namespace, id, code)
                     .then((putResponse) => {
-                        expect(putResponse).to.be.eql('OK');
-                        return storage.getCodeByCache(namespace, id, {preCache});
+                      expect(putResponse).to.be.eql('OK');
+                      return storage.getCodeByCache(namespace, id, { preCache });
                     })
                     .then((cacheResponse) => {
-                        expect(cacheResponse.preCached).to.be.true;
-                        preCache.called = false;
-                        return storage.getCodeByCache(namespace, id, {preCache});
+                      expect(cacheResponse.preCached).to.be.true;
+                      preCache.called = false;
+                      return storage.getCodeByCache(namespace, id, { preCache });
                     })
                     .then((cacheResponse) => {
-                        expect(cacheResponse.id).to.be.eql(id);
-                        expect(cacheResponse.code).to.be.eql('b = 1;');
-                        expect(cacheResponse.hash).to.be.eql('123a');
-                        expect(preCache.called).to.be.false;
-                        done();
+                      expect(cacheResponse.id).to.be.eql(id);
+                      expect(cacheResponse.code).to.be.eql('b = 1;');
+                      expect(cacheResponse.hash).to.be.eql('123a');
+                      expect(preCache.called).to.be.false;
+                      done();
                     }, (err) => {
-                        done(err);
+                      done(err);
                     });
-            });
-        });
+      });
+    });
 
-        describe('when code is found in cache and is changed', () => {
-            it('should repopulate changed item', (done) => {
-                let preCache = (code) => {
-                    code.preCachedByHash = code.hash;
-                    preCache.called = true;
-                    return code;
-                };
+    describe('when code is found in cache and is changed', () => {
+      it('should repopulate changed item', (done) => {
+        const preCache = (code) => {
+          code.preCachedByHash = code.hash;
+          preCache.called = true;
+          return code;
+        };
 
-                let namespace = 'backstage';
-                let id = 'cache-03';
-                let code = {
-                    id,
-                    code: 'c = 1;',
-                    hash: '123a',
-                };
+        const namespace = 'backstage';
+        const id = 'cache-03';
+        const code = {
+          id,
+          code: 'c = 1;',
+          hash: '123a',
+        };
 
-                storage
+        storage
                     .putCode(namespace, id, code)
                     .then((putResponse) => {
-                        expect(putResponse).to.be.eql('OK');
+                      expect(putResponse).to.be.eql('OK');
                         // populate the cache
-                        return storage.getCodeByCache(namespace, id, {preCache});
+                      return storage.getCodeByCache(namespace, id, { preCache });
                     })
                     .then((cacheResponse) => {
-                        expect(cacheResponse.preCachedByHash).to.be.eql('123a');
+                      expect(cacheResponse.preCachedByHash).to.be.eql('123a');
 
                         // change item in database
-                        code.code = 'd = 2;';
-                        code.hash = '123b';
-                        return storage.putCode(namespace, id, code);
+                      code.code = 'd = 2;';
+                      code.hash = '123b';
+                      return storage.putCode(namespace, id, code);
                     })
                     .then((putResponse) => {
-                        preCache.called = false;
-                        return storage.getCodeByCache(namespace, id, {preCache});
+                      preCache.called = false;
+                      return storage.getCodeByCache(namespace, id, { preCache });
                     })
                     .then((cacheResponse) => {
-                        expect(cacheResponse.id).to.be.eql(id);
-                        expect(cacheResponse.code).to.be.eql('d = 2;');
-                        expect(cacheResponse.preCachedByHash).to.be.eql('123b');
-                        expect(cacheResponse.hash).to.be.eql('123b');
-                        expect(preCache.called).to.be.true;
-                        done();
+                      expect(cacheResponse.id).to.be.eql(id);
+                      expect(cacheResponse.code).to.be.eql('d = 2;');
+                      expect(cacheResponse.preCachedByHash).to.be.eql('123b');
+                      expect(cacheResponse.hash).to.be.eql('123b');
+                      expect(preCache.called).to.be.true;
+                      done();
                     }, (err) => {
-                        done(err);
+                      done(err);
                     });
-            });
-        });
+      });
     });
+  });
 });
