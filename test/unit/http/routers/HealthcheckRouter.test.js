78a2e7c48e14295cf96804bc798bdc5a77bbe1cf
Wilson Júnior <wilsonpjunior@gmail.com>
2016-10-03 11:52:56 -0300

    Add test to unavailability of Storage

--- test/unit/http/routers/HealthcheckRouter.test.js
+++ test/unit/http/routers/HealthcheckRouter.test.js
@@ -1,12 +1,52 @@
+/* eslint class-methods-use-this: ['error', { "exceptMethods": ['ping']}] */
+
 const request = require('supertest');
 
 const routes = require('../../../../lib/http/routes');
+const Storage = require('../../../../lib/domain/storage');
+
+
+class WorkingStorage extends Storage {
+  ping() {
+    return new Promise((accept) => {
+      accept('PONG');
+    });
+  }
+}
+
+class NotWorkingStorage extends Storage {
+  ping() {
+    return new Promise((accept, reject) => {
+      reject(new Error('Unexpected error'));
+    });
+  }
+}
+
 
 describe('GET /healthcheck', () => {
-  it('should returns WORKING text string', (done) => {
-    request(routes)
-      .get('/healthcheck')
-      .expect('content-type', /^text\/html/)
-      .expect(200, 'WORKING', done);
+  describe('when storage is working', () => {
+    before(() => {
+      routes.set('memoryStorage', new WorkingStorage());
+    });
+
+    it('should returns WORKING text string', (done) => {
+      request(routes)
+        .get('/healthcheck')
+        .expect('content-type', /^text\/html/)
+        .expect(200, 'WORKING', done);
+    });
+  });
+
+  describe('when storage is not working', () => {
+    before(() => {
+      routes.set('memoryStorage', new NotWorkingStorage());
+    });
+
+    it('should returns ERROR text string', (done) => {
+      request(routes)
+        .get('/healthcheck')
+        .expect('content-type', /^text\/html/)
+        .expect(500, 'ERROR: Unexpected error', done);
+    });
   });
 });
