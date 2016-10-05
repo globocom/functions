5ebb1be0219d271d75071b44fdc918f74b351aae
Pedro Medeiros <pedrosnk@gmail.com>
2016-10-05 15:55:19 -0300

    Add tests for the interface Storage

---
+++ test/unit/domain/storage.test.js
@@ -0,0 +1,41 @@
+const Storage = require('../../../lib/domain/storage');
+const expect = require('chai').expect;
+
+describe('storage', () => {
+  const storage = new Storage();
+
+  describe('#ping', () => {
+    it('should throws an unimplemented error', () => {
+      const fn = () => storage.ping();
+      expect(fn).to.throw(Error, /unimplemented method for Storage interface/);
+    });
+  });
+
+  describe('#putCode', () => {
+    it('should throws an unimplemented error', () => {
+      const fn = () => storage.putCode();
+      expect(fn).to.throw(Error, /unimplemented method for Storage interface/);
+    });
+  });
+
+  describe('#getCode', () => {
+    it('should throws an unimplemented error', () => {
+      const fn = () => storage.getCode();
+      expect(fn).to.throw(Error, /unimplemented method for Storage interface/);
+    });
+  });
+
+  describe('#deleteCode', () => {
+    it('should throws an unimplemented error', () => {
+      const fn = () => storage.deleteCode();
+      expect(fn).to.throw(Error, /unimplemented method for Storage interface/);
+    });
+  });
+
+  describe('#getCodeByCache', () => {
+    it('should throws an unimplemented error', () => {
+      const fn = () => storage.getCodeByCache();
+      expect(fn).to.throw(Error, /unimplemented method for Storage interface/);
+    });
+  });
+});
