3c202047e5c1d54eb163a6122f8418810e4a9c2a
Pedro Medeiros <pedrosnk@gmail.com>
2016-10-09 13:54:22 -0300

    add tests for config discovery

---
+++ test/unit/support/config/ConfigDiscovery.test.js
@@ -0,0 +1,53 @@
+const expect = require('chai').expect;
+const ConfigDiscovery = require('../../../../lib/support/config/ConfigDiscovery');
+
+describe('ConfigDiscovery', () => {
+  describe('#getInt', () => {
+    before(() => {
+      process.env.DEFINED_GET_INT = '50';
+    });
+
+    it('should returns the default value when env is not defined', () => {
+      expect(ConfigDiscovery.getInt('NOT_DEFINED', 42)).to.be.eql(42);
+    });
+
+    it('should returns the defined env', () => {
+      expect(ConfigDiscovery.getInt('DEFINED_GET_INT', 0)).to.be.eql(50);
+    });
+  });
+
+  describe('#getList', () => {
+    before(() => {
+      process.env.DEFINED_GET_LIST = 'leftpad,  underscore  ,react';
+    });
+
+    it('should returns the default list when env is not default', () => {
+      const defaultList = ['request', 'lodash', 'leftpad'];
+      expect(ConfigDiscovery.getList('NOT_DEFINED', defaultList)).to.be.eql(defaultList);
+    });
+
+    it('should returns the defined env', () => {
+      const defaultList = ['leftpad', 'underscore', 'react'];
+      expect(ConfigDiscovery.getList('DEFINED_GET_LIST', defaultList)).to.be.eql(defaultList);
+    });
+  });
+
+  describe('#getPossibleString', () => {
+    it('should returns the default value when no env is match', () => {
+      const possibleString = ConfigDiscovery.getPossibleString(
+        ['NOT_DEFINED', 'NOT_DEFINED_2'],
+        'possiblestringvalue'
+      );
+      expect(possibleString).to.be.eql('possiblestringvalue');
+    });
+  });
+
+  describe('#parseRedisOptions', () => {
+    it('should parse the default redis options', () => {
+      const redisOptions = ConfigDiscovery.parseRedisOptions();
+      expect(redisOptions.password).to.be.eql('');
+      expect(redisOptions.sentinelName).to.be.eql('redis-cluster');
+      expect(redisOptions.keyPrefix).to.be.eql('local');
+    });
+  });
+});
