73b64b129fb1d2c60f1ac5f36710eec6ae933bd8
Mateus Pontes <mateus.pontes@corp.globo.com>
2017-05-24 15:57:12 -0300

    Add use_ssl_cert_file flag

--- test/unit/support/loadSSLCABundleFile.test.js
+++ test/unit/support/loadSSLCABundleFile.test.js
@@ -8,16 +8,25 @@ describe('CA bundle file Loader', () => {
   beforeEach(() => {
     https.globalAgent.options.ca = [];
     delete process.env.SSL_CERT_FILE;
+    delete process.env.FUNCTIONS_USE_SSL_CERT_FILE;
   });
 
   it('load the CA bundle file when a environment variable is available', () => {
+    process.env.FUNCTIONS_USE_SSL_CERT_FILE = 'true';
     process.env.SSL_CERT_FILE = path.join(__dirname, 'cacert.pem');
     expect(https.globalAgent.options.ca).to.have.lengthOf(0);
     loadCertFile();
     expect(https.globalAgent.options.ca).to.have.lengthOf(1);
   });
 
-  it('shouldnt load CA bundle file when a environment variable is not available', () => {
+  it('should not load CA bundle file whithout use flag', () => {
+    process.env.SSL_CERT_FILE = path.join(__dirname, 'cacert.pem');
+    expect(https.globalAgent.options.ca).to.have.lengthOf(0);
+    loadCertFile();
+    expect(https.globalAgent.options.ca).to.have.lengthOf(0);
+  });
+
+  it('should not load CA bundle file when a environment variable is not available', () => {
     expect(https.globalAgent.options.ca).to.have.lengthOf(0);
     loadCertFile();
     expect(https.globalAgent.options.ca).to.have.lengthOf(0);
